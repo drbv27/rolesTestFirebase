2c8fb66105961e8e13af56929f01b6e38d76017c
Diego Bonilla <drbv27@gmail.com>
2022-08-13 12:34:35 -0500

    handles submit and user registration

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,25 +1,48 @@
 import { useState } from "react"
+import firebaseApp from "../firebase/credenciales";
+import {getAuth,createUserWithEmailAndPassword} from "firebase/auth";
 
+const auth = getAuth(firebaseApp)
 
 const Login = () => {
 
-  const[isRegistrando,setIsRegistrando]=useState(false)
+  const[isRegistrando,setIsRegistrando]=useState(false);
+
+  async function registrarUsuario(email,password,rol){
+    createUserWithEmailAndPassword(auth,email,password)
+  }
+
+  function submitHandler (e) {
+    e.preventDefault();
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+    const rol = e.target.elements.rol.value;
+
+    if(isRegistrando){
+      //registrar
+      registrarUsuario(email,password,rol);
+    }else{
+      //login
+    }
+
+    console.log(email,password,rol);
+  }
 
   return (
     <div>
       <h1>{ isRegistrando ? "Registrate" : "Inicia Sesión"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <label>
           Correo electronico:
-          <input type="email"/>
+          <input type="email" id="email"/>
         </label>
         <label>
           Contraseña:
-          <input type="password"/>
+          <input type="password" id="password"/>
         </label>
         <label>
           Rol:
-          <select>
+          <select id="rol">
             <option value="admin">Administrador</option>
             <option value="user">Usuario</option>
           </select>
@@ -29,6 +52,9 @@ const Login = () => {
         value={isRegistrando ? "Registrar" : "Iniciar Sesión"}
         />
       </form>
+      <button onClick={()=> setIsRegistrando(!isRegistrando)}>
+        { isRegistrando ? "Ya tengo cuenta" : "Quiero registrarme"}
+      </button>
     </div>
   )
 }
